84c68d5413069f7d7ec8071b64a10131fe4a5c25
Valetina-2608 <valusha2608@gmail.com>
2024-08-09 14:09:17 +0300

    Add functions for input validation, add function for form submit

--- src/script1.js
+++ src/script1.js
@@ -26,20 +26,24 @@
   const auth = getAuth();
   const db = getDatabase();
 
+
   let form_sign_up = document.getElementById('form_sign');
-  form_sign_up.addEventListener('submit', registerUser);
-  function registerUser(e){
-    e.preventDefault();
+  var firstNameElem = document.getElementById('first_name');
+  var lastNameElem = document.getElementById('last_name');
+  var emailElem = document.getElementById('user_email');
+  var passwordElem =  document.getElementById('user_password');
+  var first_name_error = document.getElementById('first_name_error');
+  var last_name_error = document.getElementById('last_name_error');
+  var email_error = document.getElementById('email_error');
+  var password_error = document.getElementById('password_error');
+
+  
+  function registerUser(){
     var first_name = document.getElementById('first_name').value;
     var last_name = document.getElementById('last_name').value;
     var email = document.getElementById('user_email').value;
     var password =  document.getElementById('user_password').value;
-    var first_name_error = document.getElementById('first_name_error');
-    var last_name_error = document.getElementById('last_name_error');
-    var email_error = document.getElementById('email_error');
-    var password_error = document.getElementById('password_error');
 
-  
     createUserWithEmailAndPassword(auth, email, password)
           .then((userCredential) => {
     // Signed in 
@@ -52,14 +56,14 @@
                 password:password,
                 
           })
-          .then(() => {
+        .then(() => {
           // Data saved successfully!
-        alert('User was created successfully')
-        window.location.replace("app.html");
+            alert('User was created successfully')
+            window.location.replace("app.html");
         })
         .catch((error) => {
           // The write failed...
-    alert('Error')
+          alert('Error')
 });
   })
       .catch((error) => {
@@ -70,3 +74,80 @@
 });
 
 };
+
+
+function validationFirstName(data){
+  let pattern = /^[a-zA-Z]{3,20}$/i;
+  return pattern.test(data);
+}
+
+function validationLastName(data){
+  let pattern = /^[a-zA-Z]{5,25}$/i;
+  return pattern.test(data);
+}
+
+function validationEmail(data){
+  let pattern = /^[a-zA-Z0-9._-]+@[a-zA-Z.-]{5,30}$/i;
+  return pattern.test(data);
+}
+
+function validationPassword(data){
+  let pattern = /^[a-zA-Z0-9!@#$%^&*]{8,30}$/i;
+  return pattern.test(data);
+}
+
+firstNameElem.addEventListener('input',function(event){	
+	const elem=event.target;
+	if (!validationFirstName(elem.value)){
+		first_name_error.style.display = 'block';
+	}else{
+		first_name_error.style.display = 'none';
+}});
+
+
+lastNameElem.addEventListener('input',function(event){	
+	const elem=event.target;
+	if (!validationLastName(elem.value)){
+		last_name_error.style.display = 'block';
+	}else{
+	  last_name_error.style.display = 'none';
+}});
+
+
+emailElem.addEventListener('input',function(event){	
+	const elem=event.target;
+	if (!validationEmail(elem.value)){
+		email_error.style.display = 'block';
+	}else{
+	  email_error.style.display = 'none';
+}});
+
+passwordElem.addEventListener('input',function(event){	
+	const elem=event.target;
+	if (!validationPassword(elem.value)){
+		password_error.style.display = 'block';
+	}else{
+	  password_error.style.display = 'none';
+}});
+
+
+
+form_sign_up.addEventListener('submit', (e) => {
+  e.preventDefault();
+  var first_name = document.getElementById('first_name').value;
+  var last_name = document.getElementById('last_name').value;
+  var email = document.getElementById('user_email').value;
+  var password =  document.getElementById('user_password').value;
+
+  const isFirstNameValid=validationFirstName(first_name);
+  const isLastNameValid=validationLastName(last_name);
+  const isEmailValid=validationEmail(email);
+  const isPasswordValid=validationPassword(password);
+
+  if (isFirstNameValid && isLastNameValid  && isEmailValid && isPasswordValid){
+    registerUser();
+  }else{
+    alert('You make mistakes');
+  }
+  });
+  
