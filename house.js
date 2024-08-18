0732f5aebaa1c173e29080456356b1f491a31ffb
Valetina-2608 <valusha2608@gmail.com>
2024-08-18 18:02:35 +0300

    Add house.js

---
+++ house.js
@@ -0,0 +1,63 @@
+let words = [
+    {
+        "id":1,
+        "image":"img/attic.jpg",
+        "caption":"Attic",
+    },
+    {
+        "id":2,
+        "image":"img/backyard.jpg",
+        "caption":"Backyard",
+    },
+    {
+        "id":3,
+        "image":"img/balcony.jpg",
+        "caption":"Balcony",
+    },
+    {
+        "id":4,
+        "image":"img/bathroom.jpg",
+        "caption":"Bathroom",
+    },
+    {
+        "id":5,
+        "image":"img/bedroom.jpg",
+        "caption":"Bedroom",
+    },
+    {
+        "id":6,
+        "image":"img/dining_room.jpg",
+        "caption":"Dining room",
+    },
+    {
+        "id":7,
+        "image":"img/garage.jpg",
+        "caption":"Garage",
+    },
+    {
+        "id":8,
+        "image":"img/garden.jpg",
+        "caption":"Garden",
+    },
+    {
+        "id":9,
+        "image":"img/kitchen.jpg",
+        "caption":"Kitchen",
+    },
+    {
+        "id":10,
+        "image":"img/laundry_room.jpg",
+        "caption":"Laundry room",
+    },
+    {
+        "id":11,
+        "image":"img/living_room.jpg",
+        "caption":"Kiving room",
+    },
+    {
+        "id":12,
+        "image":"img/study_room.jpg",
+        "caption":"Study room",
+    }
+
+]
