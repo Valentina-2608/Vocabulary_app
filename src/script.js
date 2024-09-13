0ca313cf0aed8c8446417d15a34d4b06776d41ac
Valetina-2608 <valusha2608@gmail.com>
2024-09-13 17:24:42 +0300

    Change function createBlock

--- src/script.js
+++ src/script.js
@@ -1,6 +1,6 @@
 /* Creating new word_block */
 
-function createBlock(elem, elem_image, elem_caption){
+function createBlock(elem, elem_image, elem_caption, elem_text, elem_audio){
 
 	let parent = document.querySelector('.list_words');	
 	let new_block = document.createElement('div');
@@ -19,12 +19,25 @@ function createBlock(elem, elem_image, elem_caption){
     block_caption.innerHTML = elem_caption;
 	new_block.appendChild(block_caption);
 
+	/* Add text */
+	let block_text = document.createElement('div');
+	block_text.classList.add('word_block_text');
+    block_text.innerHTML = elem_text;
+	new_block.appendChild(block_text);
+
+
+	/* Add audio */
+	let block_audio = document.createElement('audio');
+	block_audio.classList.add('word_block_audio');
+    block_audio.src = elem_audio;
+	block_audio.controls = 'controls';
+	block_audio.type='audio/mpeg';
+	new_block.appendChild(block_audio);
+
 }	
 
-
-
 	for(let word of words){
-	 	createBlock(word, word.image, word.caption )
+	 	createBlock(word, word.image, word.caption, word.text, word.audio)
 }
 	
 	
