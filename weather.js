bedcc0754b723eef6b2928b7a9baebc72dd4be49
Valetina-2608 <valusha2608@gmail.com>
2024-09-15 13:00:59 +0300

    Add text and audio

--- weather.js
+++ weather.js
@@ -3,6 +3,8 @@ let words = [
         "id":1,
         "image":"img/cloudy.jpg",
         "caption":"Cloudy",
+        "text": "/ˈkɫaʊdi/",
+        "audio": "https://api.dictionaryapi.dev/media/pronunciations/en/cloudy-us.mp3",
     },
     {
         "id":2,
@@ -13,6 +15,8 @@ let words = [
         "id":3,
         "image":"img/gloomy.png",
         "caption":"Gloomy",
+        "text": "/ˈɡlumi/",
+        "audio": "https://api.dictionaryapi.dev/media/pronunciations/en/gloomy-us.mp3",
     },
     {
         "id":4,
@@ -23,6 +27,8 @@ let words = [
         "id":5,
         "image":"img/partly_cloudy.jpg",
         "caption":"Partly cloudy",
+        "text": "/ˈkɫaʊdi/",
+        "audio": "https://api.dictionaryapi.dev/media/pronunciations/en/cloudy-us.mp3",
     },
     {
         "id":6,
@@ -38,25 +44,35 @@ let words = [
         "id":8,
         "image":"img/snowy.png",
         "caption":"Snowy",
+        "text": "/snəʊi/",
+        "audio": "https://api.dictionaryapi.dev/media/pronunciations/en/snowy-us.mp3",
     },
     {
         "id":9,
         "image":"img/stormy.jpg",
         "caption":"Stormy",
+        "text": "/ˈstɔɹmi/",
+        "audio": "https://api.dictionaryapi.dev/media/pronunciations/en/stormy-us.mp3",
     },
     {
         "id":10,
         "image":"img/sunny.jpg",
         "caption":"Sunny",
+        "text": "/ˈsʌni/",
+        "audio": "https://api.dictionaryapi.dev/media/pronunciations/en/sunny-us.mp3",
     },
     {
         "id":11,
         "image":"img/thundery.png",
-        "caption":"Thundery",
+        "caption":"Thunderstorm",
+        "text": "/ˈθʌn.də(ɹ)ˌstɔː(ɹ)m/",
+        "audio": "https://api.dictionaryapi.dev/media/pronunciations/en/thunderstorm-ca.mp3",
     },
     {
         "id":12,
         "image":"img/windy.jpg",
         "caption":"Windy",
+        "text": "/ˈwɪndi/",
+        "audio": "https://api.dictionaryapi.dev/media/pronunciations/en/windy-1-au.mp3",
     }
 ]
